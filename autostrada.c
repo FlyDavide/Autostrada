a3bd837b6f3fc4bbca2cbdc2c479422caa578a42
Davide <davide.vola@mail.polimi.it>
2023-07-19 01:09:37 +0200

    Make a merge to test the final product

--- autostrada.c
+++ autostrada.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define maxArray 50;
 typedef struct nodeStation {
     int distance;
     struct nodeCars *cars;
@@ -36,6 +37,7 @@ int main(int argc, char *argv[]){
     
     char stateTurn; 
     int numLine;
+    int maxArrayAuto = maxArray;
     char readOutput; //Carattere che verrà letto dall'output, uno alla volta.
     int *arrayCars = NULL;
     int lenArrayCars = 0;
@@ -58,6 +60,7 @@ int main(int argc, char *argv[]){
             else stazione->cars = createListAutonomyCars(arrayCars, lenArrayCars);
             free(arrayCars);//cancellare l'array
             stazione = NULL;
+            maxArrayAuto = maxArray;
         }
 
         numLine = readNumOutput(); //leggo la stazione e poi verifico se esite o meno
@@ -86,34 +89,38 @@ int main(int argc, char *argv[]){
                 break;
             case 'a':
                 /* aggiungi auto */
-                station *stazione2 = searchStation( &autostrada, numLine );
-                if( stazione2 != NULL ){
-                    if( ( stazione != NULL ) && ( stazione2->distance == stazione->distance ) ){
-                        ++lenArrayCars;
-                        arrayCars = (int*) realloc( arrayCars, lenArrayCars * sizeof(int) );
-                        arrayCars[lenArrayCars - 1] = readNumOutput();
 
-                    } else if ( stazione != NULL ){
-                        heapSort(arrayCars, lenArrayCars); //oridnare l'array delle auto
-                        if( stazione->cars != NULL ) stazione->cars = insertAutnomyCars(&(stazione->cars), arrayCars, lenArrayCars); //inserisco le auto nella lista della stazione
-                        else stazione->cars = createListAutonomyCars(arrayCars, lenArrayCars);
-                        free(arrayCars);//cancellare l'array
-                        lenArrayCars = 0;
-                        stazione = NULL;
+                if( ( stazione != NULL ) && ( numLine == stazione->distance ) ){
+                    ++lenArrayCars;
+                    if( lenArrayCars > maxArrayAuto ){
+                        maxArrayAuto += maxArray;
+                        arrayCars = (int*) realloc( arrayCars, maxArrayAuto * sizeof(int) );
                     }
+                    arrayCars[lenArrayCars - 1] = readNumOutput();
 
-                    if ( stazione == NULL ){
-                        stazione = stazione2;
-                        arrayCars = (int*) malloc(sizeof(int));
+                } else if ( stazione != NULL ){
+                    heapSort(arrayCars, lenArrayCars); //oridnare l'array delle auto
+                    if( stazione->cars != NULL ) stazione->cars = insertAutnomyCars(&(stazione->cars), arrayCars, lenArrayCars); //inserisco le auto nella lista della stazione
+                    else stazione->cars = createListAutonomyCars(arrayCars, lenArrayCars);
+                    free(arrayCars);//cancellare l'array
+                    lenArrayCars = 0;
+                    stazione = NULL;
+                    maxArrayAuto = maxArray;
+                }
+
+                if ( stazione == NULL ){
+                    stazione = searchStation( &autostrada, numLine);
+                    if( stazione != NULL ){
+                        arrayCars = (int*) malloc(maxArrayAuto * sizeof(int));
                         arrayCars[0] = readNumOutput();
                         lenArrayCars = 1;
+                    } else {
+                        goToEndLine('\n');
+                        printf("non aggiunta\n");
+                        break;
                     }
-                    printf("aggiunta\n");
                 }
-                else {
-                    goToEndLine('\n');
-                    printf("non aggiunta\n");
-                }
+                printf("aggiunta\n");
                 break;
             case 'r':
                 /* rottama auto */
@@ -150,90 +157,6 @@ int main(int argc, char *argv[]){
 
 
     }
-    /*
-
-    int dati[] = {5, 84201, 32123, 12410, 8, 7, 5, 31, 9123, 2, 32, 11, 93, 6148, 8371, 61, 90, 74, 121, 52223, 754, 111, 18391, 895, 8283, 983849, 8342, 9847, 123, 9, 14, 1, 928, 12, 65, 523, 556, 56, 213, 79, 65, 304, 922, 789, 78, 7, 0, 12, 8, 3423, 5433456}; //sono i dati che dovrò inserire nell'albero BST, per verificare che funzioni
-    int lenDati = sizeof(dati)/sizeof(dati[0]);
-
-    printf("len dati: %d \n", lenDati);
-    for(int i = 0; i < lenDati ; i++){
-        insertStation(&autostrada, dati[i] );
-    }
-    //stationWalk(autostrada);
-
-    printf("\n+++ ALBERO +++\n");
-    stationWalk(autostrada);
-
-    int canc[] = {8,  31, 61, 74, 111, 5, 84201, 1, 65, 79, 0, 14, 90, 2, 52223, 895, 5433456, 754, 304, 6148, 983849, 7, 12 };
-    int lenCanc = sizeof(canc)/sizeof(canc[0]);
-    for(int i = 0; i < lenCanc; i++){
-        station *f = searcStation(&autostrada, canc[i]);
-        printf("\nCancellazione: %d\n", f->distance);
-        delateStation(&autostrada, &f);
-        stationWalk(autostrada);
-    }
-    */
-    /*
-    //voglio provare a prendere una stringa di interi e spezzarli:
-    printf("Inserire Stringa: ");
-    char c;
-    char num[12];
-    int turn = 0; //Turni: 0 -> indica che devo salvarmi la stazione; 1 -> salvarmi il numero di auto da leggere; 2 -> lettura delle auto
-    int pos = 0; //posizione di lettura dell'array num;
-    int stazione;
-    int numCars1; // rimarrà costante in modo che io conosca l'esatta dimensione dell'array
-    int numCars2; // il valore che dimunuirà 
-    int *cars = NULL;
-    while ((c = getchar()) != '\n') {
-        if(c == ' '){
-            if(turn == 0){
-                stazione = atoi(num);
-                ++turn;
-            } else if(turn == 1){
-                numCars1 = atoi(num);
-                numCars2 = numCars1;
-                cars = (int*) malloc(numCars1 * sizeof(int));
-                ++turn;
-            } else {
-                cars[numCars1 - numCars2] = atoi(num);
-                --numCars2;
-            }
-            pos = 0;
-        } else {
-            num[pos] = c;
-            ++pos;
-            num[pos] = '\0'; //Per non rischare quando userò la funzione atoi di leggere dei valori vecchi in quanto l'atori convertirebbe gli interi prima del valore '\0'
-        }
-    }
-    cars[numCars1 - 1] = atoi(num); //è il valore finale che non verrebbe letto in quanto esce dal ciclo visto che finisce con '\n' e non con ' '
-    fflush(stdout);
-
-    printf("lettura auto:\n");
-    int i = 0;
-    printf("stazione non ordinate:\n");
-    int car[] = { 1, 11, 13, 5, 78, 9, 0, 49823, 12893989, 82, 123, 3892898, 93819482, 0, 9493, 8427, 11, 1231, 5982, 838, 82, 18, 382, 7278, 87238, 4828, 547772};
-    int l = sizeof(car) / sizeof(car[0]);
-    while(i < l){
-        printf(" %d ", car[i]);
-        ++i; 
-    }
-    i = 0;
-    heapSort(car, l);
-    printf("stazione ordinate:\n");
-    while(i < l){
-        printf(" %d ", car[i]);
-        ++i; 
-    }
-    //Creazione lista:
-    printf("\nCreazione Lista:\n");
-    autonomyCars *carsList = NULL;
-    carsList = insertAutonomyCarsHead(&carsList, car, l);
-    autonomyCars *nodo = carsList; 
-    while( nodo != NULL ) {
-        printf("-> Autonomia: %d \n", nodo->autonomy );
-        nodo = nodo->next;
-    }
-    */
     printf("fine programma");
 }
 
